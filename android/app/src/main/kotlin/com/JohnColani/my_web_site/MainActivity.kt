e27f7a75b61f79dd0797d7e3ba94a0528e8dcc37
John Colani <91166301+johnhcolani@users.noreply.github.com>
2024-01-16 08:50:50 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/JohnColani/my_web_site/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.JohnColani.my_web_site
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:my_web_site/helper/sliding_menu.dart';
+import 'package:my_web_site/screens/web_screen.dart';
+
+import '../helper/app_background.dart';
+
+import 'mobile_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 500) {
+            return const Directionality(
+              textDirection: TextDirection.rtl,
+              child: Stack(
+                children: [
+                  AppBackground(),
+                  MobileScreen(),
+                  SlidingMenu(),
+
+                ],
+              ),
+            );
+          } else {
+            return const Directionality(
+              textDirection: TextDirection.rtl,
+              child: Stack(
+                children: [
+                  AppBackground(),
+                  WebScreen(),
+                  SlidingMenu(),
+
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
